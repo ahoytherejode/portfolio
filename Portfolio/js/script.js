141d052ad265ebbcb4d612bb4e37b6820a980083
jodiepage <37220706+jodiepage@users.noreply.github.com>
2018-03-15 15:32:38 +0000

    Add files via upload

---
+++ Portfolio/js/script.js
@@ -0,0 +1,13 @@
+function myFunction(x) {
+    x.classList.toggle("change");
+}
+
+window.onload = function() {
+  currentYear();
+};
+
+function currentYear(){
+  const date = new Date();
+  const autoDate = document.querySelector('#autoDate');
+  autoDate.textContent = date.getFullYear();
+};
